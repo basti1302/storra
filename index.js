882b761bd94ff4a6a2a04c447b310377912ba9b0
Bastian Krol <bastian.krol@web.de>
2013-02-26 22:15:25 +0100

    configure storra via yaml

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 #!/usr/bin/env node
 
-'use strict'
-
-var log = require('./log')
-
 /*
  * main js file of storra.
  * Use "node index.js" to start the server.
  */
 
-/* configuration */
+'use strict'
+
+require('js-yaml')
+
+var log = require('./log')
+
+/* configuration default values */
 global.storra_port = 8888
 global.storra_bind_address = "0.0.0.0"
 global.storra_backend = './backends/node_dirty_backend'
-// global.storra_backend = './backends/nstore_backend'
+
+/* load configuration from yaml */
+try {
+  var config = require('./storra.yml');
+  var config_keys = [
+    'storra_port',
+    'storra_bind_address',
+    'storra_backend'
+  ]
+  config_keys.forEach(function(key) {
+    if (config[key]) {
+      global[key] = config[key]
+      log.debug('from storra.yml: ' + key + ': ' + global[key])
+    }
+  })
+} catch (e) {
+  log.warn('Could not find configuration file storra.yml, will use default configuration values.')
+}
 
 var server = require("./server")
 
