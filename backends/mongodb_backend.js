12ada193aed9fcfdb710b60d0a3790ec9ee4f0ce
Bastian Krol <bastian.krol@web.de>
2013-03-05 22:44:25 +0100

    removed unneccessary if(err)

--- backends/mongodb_backend.js
+++ backends/mongodb_backend.js
@@ -29,132 +29,108 @@ var ObjectID = require('mongodb').ObjectID
 exports.list = function list(collectionName, writeResponse) {
   log.debug("listing " + collectionName)
   openConnection(function(err, mongoClient) {
-    if (err) {
-      writeResponse(err, null)
-    } else {
-      // TODO: We need chunking/streaming here, to write back large result sets back to the
-      // response in smaller chunks or even doc by doc, probably calling ourselves recursivly per process.nextTick to allow
-      // other requests to be handled in between the chunks, otherwise the listing of large collections
-      // block here. This also influences how requesthandler writes the response, see the TODO there.
-      withCollectionDo(mongoClient, collectionName, function(collection) {
-        var results = [] 
-        collection.find().each(function(err, doc) {
-          if (err) {
-            writeResponse(err, null)
-          } else if (doc) {
-            log.debug("listing entry: " + JSON.stringify(doc))
-            results.push(doc)
-          } else {
-            // db cursor exhausted, no more results -> write response
-            writeResponse(err, results)
-          }
-        })
+    // TODO: We need chunking/streaming here, to write back large result sets back to the
+    // response in smaller chunks or even doc by doc, probably calling ourselves recursivly per process.nextTick to allow
+    // other requests to be handled in between the chunks, otherwise the listing of large collections
+    // block here. This also influences how requesthandler writes the response, see the TODO there.
+    withCollectionDo(mongoClient, collectionName, function(collection) {
+      var results = [] 
+      collection.find().each(function(err, doc) {
+        if (err) {
+          writeResponse(err, null)
+        } else if (doc) {
+          log.debug("listing entry: " + JSON.stringify(doc))
+          results.push(doc)
+        } else {
+          // db cursor exhausted, no more results -> write response
+          writeResponse(err, results)
+        }
       })
-    }
+    })
   }, writeResponse)
 }
 
 exports.removeCollection = function removeCollection(collectionName, writeResponse) {
   log.debug("removing collection " + collectionName)
   openConnection(function(err, mongoClient) {
-    if (err) {
+    db().dropCollection(collectionName, function(err, result) {
+      // help node-mongodb-native to be idempotent, that is, ignore error if
+      // collection to be removed does not exist.
+      if (err && (err.message === 'ns not found' || err.errmsg === 'ns not found')) {
+        log.debug("Ignoring 'ns not found' error during removeCollection")
+        err = null
+      }
       writeResponse(err)
-    } else {
-      db().dropCollection(collectionName, function(err, result) {
-        // help node-mongodb-native to be idempotent, that is, ignore error if
-        // collection to be removed does not exist.
-        if (err && (err.message === 'ns not found' || err.errmsg === 'ns not found')) {
-          log.debug("Ignoring 'ns not found' error during removeCollection")
-          err = null
-        }
-        writeResponse(err)
-      })
-    }
+    })
   }, writeResponse)
 }
 
 exports.read = function read(collectionName, key, writeResponse) {
   log.debug("reading item " + collectionName + "/" + key)
   openConnection(function(err, mongoClient) {
-    if (err) {
-      writeResponse(err, null, key)
-    } else {
-      withCollectionDo(mongoClient, collectionName, function(collection) {
-        collection.findOne({_id: new ObjectID(key) }, function(err, doc) {
-          if (err) {
-            writeResponse(err, null, key)
+    withCollectionDo(mongoClient, collectionName, function(collection) {
+      collection.findOne({_id: new ObjectID(key) }, function(err, doc) {
+        if (err) {
+          writeResponse(err, null, key)
+        } else {
+          if (doc) {
+            writeResponse(err, doc, key)
           } else {
-            if (doc) {
-              writeResponse(err, doc, key)
-            } else {
-              writeResponse(404, null, key)
-            }
+            writeResponse(404, null, key)
           }
-        })
+        }
       })
-    }
+    })
   }, writeResponse)
 }
 
 exports.create = function create(collectionName, doc, writeResponse) {
   log.debug("creating item in " + collectionName)
   openConnection(function(err, mongoClient) {
-    if (err) {
-      writeResponse(err, null)
-    } else {
-      withCollectionDo(mongoClient, collectionName, function(collection) {
-        collection.insert(doc, {}, function(err, result) {
-          if (err) {
-            writeResponse(err, null)
+    withCollectionDo(mongoClient, collectionName, function(collection) {
+      collection.insert(doc, {}, function(err, result) {
+        if (err) {
+          writeResponse(err, null)
+        } else {
+          if (result) {
+            var oid = result[0]['_id'].toHexString()
+            writeResponse(err, oid)
           } else {
-            if (result) {
-              var oid = result[0]['_id'].toHexString()
-              writeResponse(err, oid)
-            } else {
-              writeResponse(err, null)
-            }
+            writeResponse(err, null)
           }
-        })
+        }
       })
-    }
+    })
   }, writeResponse)
 }
 
 exports.update = function update(collectionName, key, doc, writeResponse) {
   log.debug("updating item " + collectionName + "/" + key)
   openConnection(function(err, mongoClient) {
-    if (err) {
-      writeResponse(err, undefined)
-    } else {
-      withCollectionDo(mongoClient, collectionName, function(collection) {
-        collection.update({_id: new ObjectID(key)}, doc, {}, function(err, result) {
-          if (result == 0) {
-            writeResponse(404) 
-          } else if (result > 1) {
-            // This will never happen™.
-            writeResponse(new Error('An update changed ' + result + ' documents instead of one.'))
-          } else {
-            writeResponse(err)
-          }
-        })
+    withCollectionDo(mongoClient, collectionName, function(collection) {
+      collection.update({_id: new ObjectID(key)}, doc, {}, function(err, result) {
+        if (result == 0) {
+          writeResponse(404) 
+        } else if (result > 1) {
+          // This will never happen™.
+          writeResponse(new Error('An update changed ' + result + ' documents instead of one.'))
+        } else {
+          writeResponse(err)
+        }
       })
-    }
+    })
   }, writeResponse)
 }
 
 exports.remove = function remove(collectionName, key, writeResponse) {
   log.debug("removing item " + collectionName + "/" + key)
   openConnection(function(err, mongoClient) {
-    if (err) {
-      writeResponse(err, undefined)
-    } else {
-      withCollectionDo(mongoClient, collectionName, function(collection) {
-        collection.remove({_id: new ObjectID(key)}, function(err, numberOfRemovedDocs) {
-          log.debug("Removed " + numberOfRemovedDocs + " documents")
-          writeResponse(err)
-        })
+    withCollectionDo(mongoClient, collectionName, function(collection) {
+      collection.remove({_id: new ObjectID(key)}, function(err, numberOfRemovedDocs) {
+        log.debug("Removed " + numberOfRemovedDocs + " documents")
+        writeResponse(err)
       })
-    }
+    })
   }, writeResponse)
 }
 
