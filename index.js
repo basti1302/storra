369489c6c62be894c8c371ddc69e3cd28179fddd
Bastian Krol <bastian.krol@web.de>
2013-06-27 10:11:01 +0200

    reduce index.js to be as small as possible
    and move everything to server.js

--- index.js
+++ index.js
@@ -8,38 +8,6 @@
 'use strict';
 
 var startTime = Date.now()
-var log = require('./lib/log')
-log.info('Starting Storra...')
-
-var configReader = new (require('./lib/config_reader'))()
-configReader.read('../storra.yml')
-
 var StorraServer = require('./lib/server')
 var server = new StorraServer()
-
-/* register various handlers */
-
-// TODO !!!Use domains to handle uncaught exceptions!!!!
-
-function shutdown() {
-  log.info('Storra is about to terminate.')
-  server.shutdown()
-  log.info('Good bye.')
-}
-
-process.on('exit', function () {
-  shutdown()
-})
-
-// TODO handle more signals that are about terminating - which?
-var signals = ['SIGTERM', 'SIGINT']
-signals.forEach(function(signal) {
-  process.on(signal, function () {
-    console.log('Received signal ' + signal + ', terminating.');
-    process.exit(0)
-  })
-})
-
-log.info('using backend: ' + global.storraConfig.core.backend)
-
 server.start(startTime)
